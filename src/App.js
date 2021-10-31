c2f0241b0635a70350257a9d1ea8065091993c1e
Hossam Tarek <hossamtarek2262@gmail.com>
2021-10-31 22:55:52 +0200

    hack to delpoy while using react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import { Sketch01 } from "./sketches/sketch01";
 import { Sketch02 } from "./sketches/sketch02";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
-import { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 const App = () => {
@@ -11,11 +8,18 @@ const App = () => {
         <>
             <Navbar expand="lg">
                 <Container>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle
+                        aria-controls="basic-navbar-nav"
+                        bg="light"
+                    />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/sketch01">Sketch01</Nav.Link>
-                            <Nav.Link href="/sketch02">Sketch02</Nav.Link>
+                            <Nav.Link href="/generative-art/sketch01">
+                                Sketch01
+                            </Nav.Link>
+                            <Nav.Link href="/generative-art/sketch02">
+                                Sketch02
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -23,10 +27,16 @@ const App = () => {
             <div className="App">
                 <Router>
                     <Switch>
-                        <Route exact={true} path={["/", "/sketch01"]}>
+                        <Route
+                            exact={true}
+                            path={[
+                                "/generative-art/",
+                                "/generative-art/sketch01",
+                            ]}
+                        >
                             <Sketch01 />
                         </Route>
-                        <Route exact={true} path="/sketch02">
+                        <Route exact={true} path="/generative-art/sketch02">
                             <Sketch02 />
                         </Route>
                     </Switch>
